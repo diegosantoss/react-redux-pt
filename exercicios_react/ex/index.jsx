12c425e2a0780f938e4f4757482dd241bac054b0
Diego Santos <dsaantos1995@gmail.com>
2018-09-28 17:04:29 -0300

    new project using react redux

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { combineReducers, createStore } from "redux";
-import { Provider } from "react-redux";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-import Field from "./field";
-import fieldReducer from './fieldReducer';
+import counterReducer from './counterReducer'
+import Counter from './counter'
 
 const reducers = combineReducers({
-  field: fieldReducer
-});
+    counter: counterReducer
+})
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <Field />
-  </Provider>,
-  document.getElementById("app")
-);
+    <Provider store={createStore(reducers)}>
+        <Counter />
+    </Provider>
+, document.getElementById('app'))
--- exercicios_react/webpack.config.js
+++ exercicios_react/webpack.config.js
@@ -1,28 +1,29 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
 
 module.exports = {
-  entry: "./ex/index.jsx",
-  output: {
-    path: __dirname + "/public",
-    filename: "./bundle.js"
-  },
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
-  },
-  devServer: {
-    port: 8080,
-    contentBase: "./public"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.(js|jsx|mjs)$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        query: {
-          presets: ["es2015", "react"]
-        }
-      }
-    ]
-  }
-};
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 123455,
+        contentBase: './public',
+    },
+    // Adicionado no ex.3
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                // O plugin foi adicionado no ex.12
+                plugins: ['transform-object-rest-spread']
+            }
+        }]
+    }
+}
