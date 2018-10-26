d99ba4c3f85fbbad43b5215164b810826f97ea59
Diego Santos <dsaantos1995@gmail.com>
2018-10-26 11:48:58 -0300

    creating folders frontend app

---
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props => (
+  <div>
+    <h1>Ciclos de Pagamentos</h1>
+  </div>
+)
---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2018
+      <a href="http://diegosantos.me" target="_blank"> diegosantoss</a>.
+    </strong>
+  </footer>
+)
